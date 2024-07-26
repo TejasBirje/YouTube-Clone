5a5a4c85bb804c9c5e270a25bfb87d08cb2975d8
Tejas Birje <tejasbirje30@gmail.com>
2024-07-26 17:47:50 +0530

    Added SideBar, Videos, Feed components before API Data fetching

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,30 +1,57 @@
-import React, {useState, useEffect} from 'react'
-import { Box, Stack, Typography } from '@mui/material'
+import React, { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import {SideBar, Videos} from "./index"
 
 function Feed() {
   return (
-    <Stack sx={{flexDirection: {  
-      sx: "column",  // on small devices, show in column format
-      md: "row", // on large devices, show in row format
-    }}}>
-
-      <Box sx={{height: {
-        sx: 'auto',
-        md: '92vh'
-      }, borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2}}}>
-        SideBar
+    <Stack
+      sx={{
+        flexDirection: {
+          sx: "column", // on small devices, show in column format
+          md: "row", // on large devices, show in row format
+        },
+      }}
+    >
+      <Box
+        sx={{
+          height: {
+            sx: "auto",
+            md: "92vh",
+          },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar />
 
         {/* used instead of <p> and headings, part of MUI */}
         <Typography
-        className='copyright' variant='body2' sx={{mt: 1.5, color: "#FAF9F6"}}
-        >  
-          Copyright 2024 Tejas
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#FAF9F6" }}
+        >
+          Copyright 2024 YouTube
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{
+        overflowY: 'auto', height: '90vh', flex: 2
+      }}>
+        <Typography variant='h4' fontWeight="bold" mb={2} sx={{
+          color: "white"
+        }}>
+           
+           New <span style={{
+            color: "#fc1503"
+          }}>Videos</span>
         </Typography>
 
+          <Videos/>
+
       </Box>
-      
+
     </Stack>
-  )
+  );
 }
 
-export default Feed
+export default Feed;
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Stack } from "@mui/material";
+import { categories } from "../utils/constants";
+
+const selectedCategory = 'New'  // currently selected category
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
-  )
-}
+    <Stack
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
+  >
+      {categories.map((category) => (
+        <button
+         className="category-btn"
+         style={{
+            background: category.name === selectedCategory && '#fc1503', color: "white"  // conditionally changing the color 
+         }}
+         
 
-export default SideBar
+         key={category.name} // whenever mapping in React, we have give each property a key
+
+        >
+            {/* // One span for ICON and one for Name of category */}
+          <span style={{color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}>{category.icon}</span>
+          <span style={{
+            opacity: category.name === selectedCategory ? '1' : '0.8'  // dimming the unselected categories
+          }}>{category.name}</span>
+        </button>
+      ))}
+    </Stack>
+  );
+};
+
+export default SideBar;
